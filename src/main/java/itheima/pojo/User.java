46d0d9fd450c45ad2f3ba73dd51a2c57c67181b7
sxz <1132487537@qq.com>
2020-08-18 17:57:53 +0800

    修改User

--- src/main/java/itheima/pojo/User.java
+++ src/main/java/itheima/pojo/User.java
@@ -8,6 +8,15 @@ package itheima.pojo;
 public class User {
     private  int id;
     private  String name;
+  private String age;
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
 
     public int getId() {
         return id;
