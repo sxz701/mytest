31cd7500f0de5756c2f3a19c75190f69dfca1b85
sxz <1132487537@qq.com>
2020-08-18 17:46:18 +0800

    firstcommit

---
+++ src/main/java/itheima/pojo/User.java
@@ -0,0 +1,27 @@
+package itheima.pojo;
+
+/**
+ * @Author: sunxinzhi
+ * lbcdzyy
+ * @Date: 2020/7/30 16:26
+ */
+public class User {
+    private  int id;
+    private  String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
